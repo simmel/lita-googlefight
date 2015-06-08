740bf3fe577c28829021da6f8bedb90a64a43f54
Simon Lundström <simmel@soy.se>
2015-06-08 15:50:43 +0200

    Add code for clearing test

--- lib/lita-googlefight.rb
+++ lib/lita-googlefight.rb
@@ -3,6 +3,16 @@ require "lita"
 module Lita
   module Handlers
     class Googlefight < Handler
+
+      route(/^!g(?:oogle)?f(?:ight)?\s+(.+)/, :googlefight, help: { "!g(oogle)f(ight) term one(:|;)term two" => "GoogleFight terms against each other" })
+
+      def googlefight(response)
+        answers = response.match_data.captures.join.split(/ ?; ?/, 2)
+        require 'pp'
+        pp answers
+        response.reply("meow")
+      end
+
     end
 
     Lita.register_handler(Googlefight)
